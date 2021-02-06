c53d317c3cf0ea99f10c7a51d20b290eb3fd0c32
Bruno Wallison <brunowallison.fernandes@gmail.com>
2021-02-05 22:10:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -17,24 +17,6 @@ const files = fs.promises.readdir(`/home/bruno/Pictures/categories`).then(async 
     })
 });
 
-// const filesjp2 = fs.promises.readdir(`/home/bruno/Pictures/categories`).then(async files =>{
-//     files.map(async file=> {
-//         const filename = file.split(".")[0]
-
-//          gm(`${INPUT_FOLDER}/${file}`).write(`${filename}.jp2`,((err,success) => {
-//             if(!err){
-//                 console.warn("File successfully saved")
-//             }
-//             console.log(err)
-//         }))
-//     })
-// });
-
-
-
-
-
-
 
 console.log(`Conversion terminated sucessfully `)
 
