07f1a0f6017e0f41f3c1da31c2e21b3a29e84ba2
Bruno Wallison <brunowallison.fernandes@gmail.com>
2021-07-19 12:28:49 -0300

    fix: mkdir

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const sharp = require(`sharp`);
 const fs = require(`fs`);
 const { dirname, join } = require("path");
-const { mkdir } = require("fs/promises");
+const { mkdir, readdir } = require("fs/promises");
 const { exit } = require("process");
 
 // console.log(process.argv);
 // const INPUT_FOLDER = ;
 // const OUTPUT_FOLDER = `./images`;
 
-const handler = () => {
+const handler = async () => {
   const args = process.argv;
   const inputParamIndex = args.findIndex((arg) => arg === "-i");
   const outputParamIndex = args.findIndex((arg) => arg === "-o");
@@ -19,22 +19,27 @@ const handler = () => {
 
   if (!inputPath || !outputPath) {
     console.log("No specified input or output, exiting with status 1...");
-    exit(1);
+    exit(0);
   }
+  try {
+    const files = await readdir(join(__dirname, inputPath));
+    try {
+      await mkdir(`${join(__dirname, outputPath)}`);
+    } catch (err) { }
 
-  const files = fs.promises
-    .readdir(join(__dirname, inputPath))
-    .then(async (files) => {
+    await Promise.all(
       files.map(async (file) => {
         const filename = file.split(".")[0];
-        try {
-          await mkdir(`${join(__dirname, outputPath)}`);
-        } catch (err) {}
+        console.log(file)
         await sharp(`${join(__dirname, inputPath)}/${file}`)
           .toFormat("webp")
           .toFile(`${join(__dirname, outputPath)}/${filename}.webp`);
-      });
-    });
+      })
+    );
+  } catch (err) {
+    console.log("err", err)
+    exit(1);
+  }
 };
 
 handler();
